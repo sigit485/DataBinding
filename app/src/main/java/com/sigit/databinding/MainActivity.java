c228b49fdfdb491d1f6151ea901fbffaa18e51b6
sigit485 <sigit485@gmail.com>
2020-03-25 22:38:18 +0700

    update alert from library CFAlertDialog

--- app/src/main/java/com/sigit/databinding/MainActivity.java
+++ app/src/main/java/com/sigit/databinding/MainActivity.java
@@ -8,10 +8,12 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.crowdfire.cfalertdialog.CFAlertDialog;
 import com.sigit.databinding.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity implements MainNavigator{
+public class MainActivity extends AppCompatActivity implements MainNavigator {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,19 +33,37 @@ public class MainActivity extends AppCompatActivity implements MainNavigator{
 
     @Override
     public void onItemClick(User user) {
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-            alert.setMessage(user.name + "\n" + user.email);
-               if (user.isMark.get()){
-                    alert.setPositiveButton("UNMARK", (dialog, which) -> {
-                       user.isMark.set(false);
-                       dialog.dismiss();
-                   });
-               } else {
-                   alert.setPositiveButton("MARK", (dialog, which) -> {
-                       user.isMark.set(true);
-                       dialog.dismiss();
-                   });
-               }
-                alert.show();
+//        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+//            alert.setMessage(user.name + "\n" + user.email);
+//               if (user.isMark.get()){
+//                    alert.setPositiveButton("UNMARK", (dialog, which) -> {
+//                       user.isMark.set(false);
+//                       dialog.dismiss();
+//                   });
+//               } else {
+//                   alert.setPositiveButton("MARK", (dialog, which) -> {
+//                       user.isMark.set(true);
+//                       dialog.dismiss();
+//                   });
+//               }
+//                alert.show();
+
+        // Create Alert using Builder
+        CFAlertDialog.Builder alert = new CFAlertDialog.Builder(this);
+        alert.setMessage(user.name + "\n" + user.email);
+        if (user.isMark.get()) {
+            alert.addButton("UNMARK", -1, -1, CFAlertDialog.CFAlertActionStyle.POSITIVE, CFAlertDialog.CFAlertActionAlignment.END, (dialog, which) -> {
+                Toast.makeText(MainActivity.this, "Unmark Item Selected", Toast.LENGTH_SHORT).show();
+                user.isMark.set(false);
+                dialog.dismiss();
+            });
+        } else {
+            alert.addButton("MARK", -1, -1, CFAlertDialog.CFAlertActionStyle.POSITIVE, CFAlertDialog.CFAlertActionAlignment.END, (dialog, which) -> {
+                Toast.makeText(MainActivity.this, "Mark Item Selected", Toast.LENGTH_SHORT).show();
+                user.isMark.set(true);
+                dialog.dismiss();
+            });
+        }
+        alert.show();
     }
 }
